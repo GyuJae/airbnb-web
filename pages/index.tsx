0370764d459fc88c4783e26940c0b23d510954bf
Gyujae <wjdrbwo1206@naver.com>
2022-02-26 20:50:37 +0900

    Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
 import type { NextPage } from "next";
-import Head from "next/head";
+import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
+  const router = useRouter();
   return (
     <Layout title={"Home"}>
-      <div className="">
-        <h1>Home</h1>
+      <div className="grid grid-cols-4 gap-9">
+        {[
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        ].map((i) => (
+          <div
+            key={i}
+            onClick={() => router.push(`/rooms/${i}`)}
+            className="space-y-1 cursor-pointer"
+          >
+            <div className="w-[300px] h-[300px] bg-gray-200 rounded-xl"></div>
+            <div className="flex justify-between items-center">
+              <div className="font-semibold">location</div>
+              <div className="text-sm">\price / 박</div>
+            </div>
+            <div className="flex justify-between items-center text-gray-500">
+              <div className="text-sm font-medium">distance</div>
+              <div className="text-xs">12월6일 ~ 17일</div>
+            </div>
+          </div>
+        ))}
       </div>
     </Layout>
   );
